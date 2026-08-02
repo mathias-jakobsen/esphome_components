a78a7bda23a1d8afa400db80abc2fa96ac387551
mathias-jakobsen <55285073+mathias-jakobsen@users.noreply.github.com>
2026-08-02 12:04:03 +0200

    Fix string quotes in sensor.py schema for temperature option

--- components/wavin_ahc9000/sensor.py
+++ components/wavin_ahc9000/sensor.py
@@ -11,16 +11,16 @@ from esphome.const import (
 
 from . import wavin_ns, WavinAHC9000Component
 
-CONF_WAVIN_ID = wavin_ahc9000_id
-CONF_CHANNEL = channel
+CONF_WAVIN_ID = "wavin_ahc9000_id"
+CONF_CHANNEL = "channel"
 
-WavinZoneTemperatureSensor = wavin_ns.class_(WavinZoneTemperatureSensor, sensor.Sensor, cg.Component)
-WavinZoneBatterySensor = wavin_ns.class_(WavinZoneBatterySensor, sensor.Sensor, cg.Component)
-WavinZoneRSSISensor = wavin_ns.class_(WavinZoneRSSISensor, sensor.Sensor, cg.Component)
+WavinZoneTemperatureSensor = wavin_ns.class_("WavinZoneTemperatureSensor", sensor.Sensor, cg.Component)
+WavinZoneBatterySensor = wavin_ns.class_("WavinZoneBatterySensor", sensor.Sensor, cg.Component)
+WavinZoneRSSISensor = wavin_ns.class_("WavinZoneRSSISensor", sensor.Sensor, cg.Component)
 
 CONFIG_SCHEMA = cv.typed_schema(
     {
-        temperature: sensor.sensor_schema(
+        "temperature": sensor.sensor_schema(
             WavinZoneTemperatureSensor,
             unit_of_measurement=UNIT_CELSIUS,
             accuracy_decimals=1,
@@ -32,13 +32,13 @@ CONFIG_SCHEMA = cv.typed_schema(
                 cv.Required(CONF_CHANNEL): cv.int_range(min=1, max=16),
             }
         ).extend(cv.COMPONENT_SCHEMA),
-        battery: sensor.sensor_schema(WavinZoneBatterySensor).extend(
+        "battery": sensor.sensor_schema(WavinZoneBatterySensor).extend(
             {
                 cv.GenerateID(CONF_WAVIN_ID): cv.use_id(WavinAHC9000Component),
                 cv.Required(CONF_CHANNEL): cv.int_range(min=1, max=16),
             }
         ).extend(cv.COMPONENT_SCHEMA),
-        rssi: sensor.sensor_schema(WavinZoneRSSISensor).extend(
+        "rssi": sensor.sensor_schema(WavinZoneRSSISensor).extend(
             {
                 cv.GenerateID(CONF_WAVIN_ID): cv.use_id(WavinAHC9000Component),
                 cv.Required(CONF_CHANNEL): cv.int_range(min=1, max=16),
@@ -52,9 +52,9 @@ async def to_code(config):
     hub = await cg.get_variable(config[CONF_WAVIN_ID])
     ch = config[CONF_CHANNEL]
     sens_type = config[CONF_TYPE]
-    if sens_type == temperature:
+    if sens_type == "temperature":
         var = cg.new_Pvariable(config[CONF_ID], hub, ch)
-    elif sens_type == battery:
+    elif sens_type == "battery":
         var = cg.new_Pvariable(config[CONF_ID], hub, ch)
     else:
         var = cg.new_Pvariable(config[CONF_ID], hub, ch)
