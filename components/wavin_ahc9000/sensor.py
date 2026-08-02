f717c1818d94ea977ccd797393488677ae6ea327
mathias-jakobsen <55285073+mathias-jakobsen@users.noreply.github.com>
2026-08-02 11:55:22 +0200

    Add cg.register_component to components/wavin_ahc9000/sensor.py to register entities as active components

--- components/wavin_ahc9000/sensor.py
+++ components/wavin_ahc9000/sensor.py
@@ -18,13 +18,13 @@ CONFIG_SCHEMA = cv.typed_schema(
                 cv.GenerateID(CONF_WAVIN_ID): cv.use_id(WavinAHC9000Component),
                 cv.Required(CONF_CHANNEL): cv.int_range(min=1, max=16),
             }
-        ),
+        ).extend(cv.COMPONENT_SCHEMA),
         "rssi": sensor.sensor_schema(WavinZoneRSSISensor).extend(
             {
                 cv.GenerateID(CONF_WAVIN_ID): cv.use_id(WavinAHC9000Component),
                 cv.Required(CONF_CHANNEL): cv.int_range(min=1, max=16),
             }
-        ),
+        ).extend(cv.COMPONENT_SCHEMA),
     },
     lower=True,
 )
@@ -37,4 +37,5 @@ async def to_code(config):
         var = cg.new_Pvariable(config[CONF_ID], hub, ch)
     else:
         var = cg.new_Pvariable(config[CONF_ID], hub, ch)
+    await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
